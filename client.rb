a7b45e7fa2ebeae08702eb1bc9c034107bef2daf
pavanganti <pavanganti@gmail.com>
2017-03-22 18:25:54 -0400

    initial commit

---
+++ client.rb
@@ -0,0 +1,5 @@
+log_level        :info
+log_location     STDOUT
+chef_server_url  'https://ec2-35-165-52-110.us-west-2.compute.amazonaws.com/organizations/mypersonal'
+validation_key         "/etc/chef/pavanganti.pem"
+validation_client_name 'pavanganti'
