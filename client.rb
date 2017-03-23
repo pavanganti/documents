7b057d428d190ceb8bf2add48d644dad6db7cf54
pavanganti <pavanganti@gmail.com>
2017-03-23 00:51:50 -0400

    Update client.rb

--- client.rb
+++ client.rb
@@ -1,5 +1,5 @@
 log_level        :info
 log_location     STDOUT
-chef_server_url  'https://ec2-35-165-52-110.us-west-2.compute.amazonaws.com/organizations/mypersonal'
+chef_server_url  'https://ec2-34-208-8-107.us-west-2.compute.amazonaws.com/organizations/mypersonal'
 validation_key         "/etc/chef/pavanganti.pem"
 validation_client_name 'pavanganti'
